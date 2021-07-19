8bac111d1ae2f7c2a70ec62f42063d7bc4773e30
Dawid Kłos <klosdawid12@gmail.com>
2021-07-19 16:57:32 +0100

    Feat: Theme changer for 3 different styles

--- src/js/index.js
+++ src/js/index.js
@@ -64,20 +64,30 @@ buttons.addEventListener("click", (event) => {
   }
 
   if (target.classList.contains("digit")) {
+    
+    if (calculator.currentDisplay.includes('.') && target.value == ".") {
+      console.log("1");
+      return;
+    }
+
     if (calculator.currentDisplay == "0" && target.value == ".") {
+      console.log("2");
       target.value = "0.";
     }
 
     if (calculator.currentDisplay == "0" && target.value == "0") {
       calculator.currentDisplay = "0";
+      console.log("3");
       updateResultOnDisplay();
       return;
     }
 
     if (calculator.currentDisplay == "0" && target.value !== "0") {
+      console.log("4");
       calculator.currentDisplay = target.value;
     } else {
       calculator.currentDisplay += target.value;
+      console.log("4");
     }
 
     updateResultOnDisplay();
@@ -116,5 +126,74 @@ buttons.addEventListener("click", (event) => {
     calculator.currentDisplay = "0";
     updateResultOnDisplay();
     return;
+  }
+});
+
+// Theme changing
+
+const theme = document.querySelector(".theme--js");
+const body = document.querySelector(".body");
+const display = document.querySelector(".result");
+const allBtn = document.querySelectorAll(".btn");
+const btnDel = document.querySelector(".btn-del");
+const btnReset = document.querySelector(".btn-reset");
+const btnEqual = document.querySelector(".btn-equal");
+
+function changeButtonsStyle(bg, shadow, font) {
+  for (let i = 0; i < allBtn.length; i++) {
+    allBtn[i].style.background = bg;
+    allBtn[i].style.boxShadow = shadow;
+    allBtn[i].style.color = font;
+  }
+}
+
+theme.addEventListener('change' , (event) => {
+  switch (event.target.value) {
+    case "0":
+      body.style.background = "#3b4664";
+      body.style.color = "#fffeff";
+      theme.style.background = "#252d44";
+      display.style.background = "#181f32";
+      buttons.style.background = "#252d44";
+      changeButtonsStyle("#eae3d9","inset  0px -4px #b2a89e", "#414852");
+      btnDel.style.background = "#647299";
+      btnDel.style.boxShadow = "inset  0px -4px #495061";
+      btnReset.style.background = "#647299";
+      btnReset.style.boxShadow = "inset  0px -4px #495061";
+      btnEqual.style.background = "#c94431";
+      btnEqual.style.boxShadow = "inset  0px -4px #793329";
+      btnEqual.style.color = "#fffeff";
+
+      break;
+    case "1":
+      body.style.background = "#e6e6e6";
+      body.style.color = "#34342a";
+      theme.style.background = "#d3cdcd";
+      display.style.background = "#eeeeee";
+      buttons.style.background = "#d3cdcd";
+      changeButtonsStyle("#e5e4e0","inset  0px -4px #a69c90", "#414852");
+      btnDel.style.background = "#388187";
+      btnDel.style.boxShadow = "inset  0px -4px #1a5d66";
+      btnReset.style.background = "#388187";
+      btnReset.style.boxShadow = "inset  0px -4px #1a5d66";
+      btnEqual.style.background = "#c75403";
+      btnEqual.style.boxShadow = "inset  0px -4px #893a02";
+      btnEqual.style.color = "#fffeff";
+      break;
+    case "2":
+      body.style.background = "#17062a";
+      body.style.color = "#ffdf3f";
+      theme.style.background = "#1e0836";
+      display.style.background = "#1e0836";
+      buttons.style.background = "#1e0836";
+      changeButtonsStyle("#331b4d","inset  0px -4px #851c9d", "#ffdf3f");
+      btnDel.style.background = "#56077c";
+      btnDel.style.boxShadow = "inset  0px -4px #bf16f5";
+      btnReset.style.background = "#56077c";
+      btnReset.style.boxShadow = "inset  0px -4px #bf16f5";
+      btnEqual.style.background = "#00decf";
+      btnEqual.style.boxShadow = "inset  0px -4px #6ef7ef";
+      btnEqual.style.color = "black";
+      break;
   }
 });
